7170c060af988b2b0ff64a35d103ceda4b729121
hatice171717 <haticekbr@gmail.com>
2023-02-04 01:07:33 +0300

    Api katmanı ile proje katmanı arasında bağlantı sağlandı.

--- BlogApi/DataAccessLayer/Context.cs
+++ BlogApi/DataAccessLayer/Context.cs
@@ -6,8 +6,8 @@ namespace BlogApi.DataAccessLayer
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=302-03;Database=MvcCore_ProKamp150_Api;User Id=WebMobile_302;Password=WebMobile_302;");
-            //optionsBuilder.UseSqlServer("Server=DESKTOP-FT6FLSV\\SQLEXPRESS;Database=MvcCore_ProKamp150;integrated security=true;");
+            //optionsBuilder.UseSqlServer("Server=302-03;Database=MvcCore_ProKamp150_Api;User Id=WebMobile_302;Password=WebMobile_302;");
+            optionsBuilder.UseSqlServer("Server=DESKTOP-FT6FLSV\\SQLEXPRESS;Database=MvcCore_ProKamp150_Api;integrated security=true;");
         }
         public DbSet<Employee> Employees { get; set; }
 
---
+++ ICore/Controllers/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace A_ICore.Controllers
+{
+    [AllowAnonymous]
+    public class EmployeeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.GetAsync("https://localhost:7139/api/Default");
+            var jsonString = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<Class1>>(jsonString);
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(Class1 p)
+        {
+            var httpClient = new HttpClient();
+            var jsonEmployee = JsonConvert.SerializeObject(p);
+            StringContent content = new StringContent(jsonEmployee, Encoding.UTF8, "application/json");
+            var responseMessage = await httpClient.PostAsync("https://localhost:7139/api/Default", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(p);
+        }
+    }
+    public class Class1
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
