3495d54939b472dea0687ac54ab1b7e8a00d72fa
hatice171717 <haticekbr@gmail.com>
2023-02-15 23:55:51 +0300

    Admin Paneli gelen kutusu giden kutusu

--- ICore/Areas/Admin/Controllers/AdminMessageController.cs
+++ ICore/Areas/Admin/Controllers/AdminMessageController.cs
@@ -1,15 +1,33 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace A_ICore.Areas.Admin.Controllers
 {
     [Area("Admin")]
-    [AllowAnonymous]
+    //[AllowAnonymous]
     public class AdminMessageController : Controller
     {
+        Message2Manager mm = new Message2Manager(new EfMessage2Repository());
+        Context c = new Context();
         public IActionResult Inbox()
         {
-            return View();
+            var username=User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerID=c.Writers.Where(x=>x.WriterMail==usermail).Select(y => y.WriterID).FirstOrDefault();
+            var values = mm.GetInboxListWriter(writerID);
+            return View(values);
         }
+        public IActionResult SendBox()
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+            var values = mm.GetSendBoxListWriter(writerID);
+            return View(values);
+        }
+
     }
 }
