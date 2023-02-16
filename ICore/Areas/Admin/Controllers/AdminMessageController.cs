276c200600f751f32cecedc4489857ace6c8c34c
hatice171717 <haticekbr@gmail.com>
2023-02-16 21:09:37 +0300

    ComposeMessage tamamlandı. Admin Blog Listesi tasarım yapıldı. Listeleme yapıldı

--- ICore/Areas/Admin/Controllers/AdminMessageController.cs
+++ ICore/Areas/Admin/Controllers/AdminMessageController.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,9 +15,9 @@ namespace A_ICore.Areas.Admin.Controllers
         Context c = new Context();
         public IActionResult Inbox()
         {
-            var username=User.Identity.Name;
+            var username = User.Identity.Name;
             var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
-            var writerID=c.Writers.Where(x=>x.WriterMail==usermail).Select(y => y.WriterID).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
             var values = mm.GetInboxListWriter(writerID);
             return View(values);
         }
@@ -28,11 +29,23 @@ namespace A_ICore.Areas.Admin.Controllers
             var values = mm.GetSendBoxListWriter(writerID);
             return View(values);
         }
-     
+        [HttpGet]
         public IActionResult ComposeMessage()
         {
             return View();
         }
-
+        [HttpPost]
+        public IActionResult ComposeMessage(Message2 p)
+        {
+            var username = User.Identity.Name;
+            var usermail = c.Users.Where(x => x.UserName == username).Select(y => y.Email).FirstOrDefault();
+            var writerID = c.Writers.Where(x => x.WriterMail == usermail).Select(y => y.WriterID).FirstOrDefault();
+            p.SenderID = writerID;
+            p.ReceiverID = 2;
+            p.MessageDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            p.MessageStatus = true;
+            mm.TAdd(p);
+            return RedirectToAction("SendBox");
+        }
     }
 }
